6dcbe139620d6335ad231a53cd73738a4efb3c8d
Colin Rodrigues <colin.rods@gmail.com>
2024-09-30 13:19:35 +0200

    commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 alphabet = pd.read_csv('nato_phonetic_alphabet.csv')
 
-alphabet_dict = {row['letter']: row['code'] for _, row in alphabet.iterrows()}
+alphabet_dict = {row.letter: row.code for (index,row) in alphabet.iterrows()}
 
 user_input = input("What word would you like to convert?").upper()
 converted_output = [alphabet_dict[letter] for letter in user_input]
