e225ceab82c70184e80a6f8fb0eced28aba7dcfe
Colin Rodrigues <colin.rods@gmail.com>
2024-10-07 14:08:58 +0200

    commit

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ alphabet = pd.read_csv('nato_phonetic_alphabet.csv')
 
 alphabet_dict = {row.letter: row.code for (index,row) in alphabet.iterrows()}
 
-user_input = input("What word would you like to convert?").upper()
-converted_output = [alphabet_dict[letter] for letter in user_input]
-print(converted_output)
+def generate_phonetic():
+    user_input = input("What word would you like to convert?").upper()
+    try:
+        converted_output = [alphabet_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(converted_output)
+
+
+generate_phonetic()
